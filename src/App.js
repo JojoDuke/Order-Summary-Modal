bb990d0ca56e21d6d11bc33f199f5cb330ea6a88
JojoDuke <jojoamankwa@gmail.com>
2021-10-09 15:50:17 +0000

    Getting somewhere

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import './App.css';
+import { Button, Card, CardActions, CardContent, Typography } from '@mui/material';
+import useStyles from './styled';
+import heroImage from './images/illustration-hero.svg';
+import musicIcon from './images/icon-music.svg';
+import zIndex from '@mui/material/styles/zIndex';
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Hello to the World!!</h1>
+    <div className={classes.container}>
+      <div/>
+      
+      <div>
+        <Card>
+          <CardContent>
+            <div className={classes.heroImage}>
+              <img src={heroImage} alt="party" />
+            </div>
+
+            <Typography variant="h4">Order Summary</Typography>
+            <Typography variant="h6">You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</Typography>
+
+            {/* Annual Plan */}
+            <div className={classes.annual}>
+              <img src={musicIcon} alt="music-icon" />
+              <Typography variant="h5">Annual Plan</Typography>
+              <Typography variant="h6">$59.99/year</Typography>
+              <Button variant="text">Change</Button>
+            </div>
+
+            {/* Buttons */}
+            <CardActions>
+              <Button variant="contained">Proceed to Payment</Button>
+              <Button variant="text">Cancel Order</Button>
+            </CardActions>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
---
+++ src/styled.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles((theme) => ({
+    container:{
+        backgroundColor: "#D6E1FF",
+    }
+}));
+
+export default useStyles;
